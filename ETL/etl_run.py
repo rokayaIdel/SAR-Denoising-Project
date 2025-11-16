d897e38c3cb076ceb2b6bb42045f2494f7013b80
MSI Mehdi <137456035+oliverqueen14@users.noreply.github.com>
2025-11-16 05:09:32 +0100

    added get started with pickle data + uncommented etl_run file + cleaned medianFilter file

--- ETL/etl_run.py
+++ ETL/etl_run.py
@@ -4,27 +4,27 @@ from src.load.load import Load
 import shutil
 import os
 if __name__ == "__main__":
-    # print("=== ETL PROCESS STARTED ===")
-    # print("")
-    # print("CLEANING PREVIOUS DATA IF ANY...")
-    # dirs_to_clean = ["data/processed", "data/patches", "data/pickles", "data/raw"]
-    # for dir_name in dirs_to_clean:
-    #     if os.path.exists(dir_name):
-    #         shutil.rmtree(dir_name)
-    # print("CLEANING DONE.")
-    # print("")
+    print("=== ETL PROCESS STARTED ===")
+    print("")
+    print("CLEANING PREVIOUS DATA IF ANY...")
+    dirs_to_clean = ["data/processed", "data/patches", "data/pickles", "data/raw"]
+    for dir_name in dirs_to_clean:
+        if os.path.exists(dir_name):
+            shutil.rmtree(dir_name)
+    print("CLEANING DONE.")
+    print("")
     
-    # print("=== EXTRACT ===")
-    # Extract.download_datasets()
+    print("=== EXTRACT ===")
+    Extract.download_datasets()
 
-    # print("=== TRANSFORM ===")
-    # # delete data/processed, data/patches and data/pickles folders if they exist
+    print("=== TRANSFORM ===")
+    # delete data/processed, data/patches and data/pickles folders if they exist
 
-    # dirs_to_clean = ["data/processed", "data/patches", "data/pickles"]
-    # for dir_name in dirs_to_clean:
-    #     if os.path.exists(dir_name):
-    #         shutil.rmtree(dir_name)
-    # Transform.generate_patches_for_all_images("data/raw", patch_size=64, stride=32, L=3)
+    dirs_to_clean = ["data/processed", "data/patches", "data/pickles"]
+    for dir_name in dirs_to_clean:
+        if os.path.exists(dir_name):
+            shutil.rmtree(dir_name)
+    Transform.generate_patches_for_all_images("data/raw", patch_size=64, stride=32, L=3)
 
     dirs_to_clean = ["data/pickles"]
     for dir_name in dirs_to_clean:
